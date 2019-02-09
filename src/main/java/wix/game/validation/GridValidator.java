5e53f4b5f3795f37ce38bdde79029b359ff3728b
Yaroslav Ishchenko <yaroslav.ishchenko@zoomdata.com>
2019-02-09 14:53:14 +0200

    * Validation finished + test coverage * additional dependencies for lombok, log4j

--- src/main/java/wix/game/validation/GridValidator.java
+++ src/main/java/wix/game/validation/GridValidator.java
@@ -1,32 +1,54 @@
 package wix.game.validation;
 
-public class GridValidator implements IGridValidator {
-    int width;
-    int height;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
 
-    public GridValidator(int width, int height) {
-        this.width = width;
-        this.height = height;
-    }
+@Log4j
+@AllArgsConstructor
+public class GridValidator implements IGridValidator {
+    private final int width;
+    private final int height;
 
     @Override
-    public boolean isValid(Integer[] board) { // generify with comparator
+    public boolean isValid(Integer[] board) {
         int length = width * height;
         int totalInversions = 0;
-            for (int i = 0; i < length - 1; i++) {
-                int inversion = 0;
-                for (int j = i + 1; j < length - 2; j++) {
-                    if (board[i] > board[j]) {
-                        inversion++;
-                    }
-                }
-                totalInversions += inversion;
-            }
-            //( (grid width odd) && (#inversions even) )  ||  ( (grid width even) && ((blank on odd row from bottom) == (#inversions even)) )
-            boolean gridWithEven = width % 2 == 0;
-            boolean inversionsEven = totalInversions % 2 == 0;
-            boolean blankOnOddRow = height % 2 == 0;
+        int blankPosition = 0;
+        int rowNum = 0;
+        //count inversions for every element in the sequence
+        for (int i = 0; i < length - 1; i++) {
+            int inversion = 0; // count inversions to the right from current element position
 
-            return gridWithEven && blankOnOddRow && inversionsEven|| (!gridWithEven && inversionsEven);
+            if (i % width == 0) { // count row
+                rowNum++;
+            }
+
+            if (board[i] == 0) { // the blank tile
+                blankPosition = rowNum; // save the row on which encountered
+                continue;
+            }
+
+            for (int j = i + 1; j < length; j++) {
+                if (board[i] > board[j] && board[j] != 0) {
+                    inversion++;
+                }
+            }
+            totalInversions += inversion;
+        }
+        //( (grid width odd) && (#inversions even) )  ||  ( (grid width even) && ((blank on odd row from bottom) == (#inversions even)) )
+        log.info("Total Inversions Count= " + totalInversions + "");
+        boolean gridWithEven = width % 2 == 0;
+        boolean inversionsEven = totalInversions % 2 == 0;
+        boolean blankOnEvenRow = blankPosition % 2 == 0;
+
+        if (gridWithEven) {
+            if (blankOnEvenRow) {
+                return inversionsEven;
+            } else {
+                return !inversionsEven;
+           }
+        } else {
+            return inversionsEven;
+        }
     }
 }
--- src/test/java/wix/game/validation/GridValidatorTest.java
+++ src/test/java/wix/game/validation/GridValidatorTest.java
@@ -3,10 +3,11 @@ package wix.game.validation;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class GridValidatorTest {
-    GridValidator gridValidator;
+    private GridValidator gridValidator;
 
     @Before
     public void setUp() {
@@ -14,9 +15,34 @@ public class GridValidatorTest {
     }
 
     @Test
-    public void testOnEvenRowsGrid() {
-        Integer[] sequence = {12, 1, 10, 2, 7, 11, 4, 14, 5, 9, 15, 8, 13, 6, 3, 0};
+    public void testEvenWidthBlankEvenRowInversionsEvenGridValid() {
+        Integer[] sequence = {3, 9, 1, 15, 14, 11, 4, 6, 13, 10, 12, 2, 7, 8, 5, 0};
         assertTrue(gridValidator.isValid(sequence));
     }
 
+    @Test
+    public void testEvenWidthBlankEvenRowInversionsOddGridInvalid() {
+        Integer[] sequence = {12, 1, 10, 2, 7, 11, 4, 14, 5, 9, 15, 8, 13, 6, 3, 0};
+        assertFalse(gridValidator.isValid(sequence));
+    }
+
+    @Test
+    public void testEvenWidthBlankOddRowInversionsOddGridValid() {
+        Integer[] sequence = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 15, 13, 14, 12};
+        assertTrue(gridValidator.isValid(sequence));
+    }
+
+    @Test
+    public void testOddWidthBlankOddRowInversionsEvenGridValid() {
+        gridValidator = new GridValidator(3, 3);
+        Integer[] sequence = {1, 8, 2, 5, 4, 3, 7, 6, 0};
+        assertTrue(gridValidator.isValid(sequence));
+    }
+
+    @Test
+    public void testOddWidthBlankEvenRowInversionsOddGridInalid() {
+        gridValidator = new GridValidator(3, 3);
+        Integer[] sequence = {1, 8, 2, 5, 4, 3, 6, 7, 0};
+        assertFalse(gridValidator.isValid(sequence));
+    }
 }
